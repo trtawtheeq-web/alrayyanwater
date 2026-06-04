fd5b3f3a9602d3d1e10d36e944ba43cb8d7f282e
fanarali881-eng <2.52206377e+08+fanarali881-eng@users.noreply.github.com>
2026-06-04 09:06:16 +0000

    fix: hide menu-collapser on desktop + fix duplicate carousel indicators

--- client/public/rayyan-site/assets/js/main.js
+++ client/public/rayyan-site/assets/js/main.js
@@ -96,11 +96,14 @@ var menu= $("#mega-menu-holder");
   let heroCarouselIndicators = select("#hero-carousel-indicators")
   let heroCarouselItems = select('#heroCarousel .carousel-item', true)
 
-  heroCarouselItems.forEach((item, index) => {
-    (index === 0) ?
-    heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "' class='active'></li>":
-      heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "'></li>"
-  });
+  if (heroCarouselIndicators) {
+    heroCarouselIndicators.innerHTML = '';
+    heroCarouselItems.forEach((item, index) => {
+      (index === 0) ?
+      heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "' class='active'></li>":
+        heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "'></li>"
+    });
+  }
 
     /**
    * Gallery isotope and filter
